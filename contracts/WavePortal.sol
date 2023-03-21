845bfeebc22c2a630d3a669fd858865b4333af75
kk0917 <kamenashi0917@gmail.com>
2023-03-21 17:21:27 +0900

    feat: add sending message code when sending wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,13 +8,28 @@ import "hardhat/console.sol";
 contract WavePortal {
   uint256 totalWaves;
 
-  constructor() {
-    console.log("Here is my first smart contract!");
+  event NewWave(address indexed from, uint256 timestamp, string message);
+
+  struct Wave {
+    address waver;
+    string message;
+    uint256 timestamp;
   }
 
-  function wave() public {
+  Wave[] waves;
+  constructor() {
+    console.log("WavePortal - Smart Contract!");
+  }
+
+  function wave(string memory _message) public {
     totalWaves += 1;
-    console.log("%s has waved!", msg.sender);
+    console.log("%s waved w/ message %s", msg.sender, _message);
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+    emit NewWave(msg.sender, block.timestamp, _message);
+  }
+
+  function getAllWaves() public view returns (Wave[] memory) {
+    return waves;
   }
 
   function getTotalWaves() public view returns (uint256) {
